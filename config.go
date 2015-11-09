198d33a5b75cb22e26454aa8e1052b2d59433992
Jean-Loup Jamet <jean-loup.jamet@parrot.com>
2015-11-09 18:15:37 +0100

    little helper for toml config file

---
+++ config.go
@@ -0,0 +1,41 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Conf interface {
+	SetToDefaults()
+}
+
+func MustParseConfigFile(path string, conf Conf) {
+	conf.SetToDefaults()
+	MustDecodeConfigFile(path, conf)
+}
+
+func MustDecodeConfigFile(path string, conf interface{}) {
+	m, err := toml.DecodeFile(path, conf)
+	if err != nil {
+		panic(err)
+	}
+
+	s := reflect.TypeOf(conf).Elem()
+	var missingParams []string
+	for i := 0; i < s.NumField(); i++ {
+		field := s.Field(i)
+		if field.Tag.Get("conf") != "required" {
+			continue
+		}
+		if !m.IsDefined(strings.ToLower(field.Name)) {
+			missingParams = append(missingParams, strings.ToLower(field.Name))
+		}
+	}
+
+	if len(missingParams) != 0 {
+		panic(fmt.Sprintf("following config parameters are missing: %s", strings.Join(missingParams, ", ")))
+	}
+}
